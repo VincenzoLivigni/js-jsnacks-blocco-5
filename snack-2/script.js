8b3db4863bdc133cfa0f6fbafb21e78b0358ec23
VincenzoLivigni <enzolivigni13@gmail.com>
2025-10-22 14:14:11 +0200

    snack 2 done

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,22 @@
+// Stampa in console tutti i nomi
+// Risultato: 'Paolo', 'Giulia', 'Marco'
+
 const people = [
   { name: 'Paolo', age: 35 },
   { name: 'Giulia', age: 24 },
   { name: 'Marco', age: 67 }
 ];
 
-// Stampa in console tutti i nomi
-// Risultato: 'Paolo', 'Giulia', 'Marco'
+console.log(people);
+
+// for 
+
+for (let i = 0; i < people.length; i++) {
+     const person = people[i];
+     console.log(person.name);
+}
+
+// forEach
+people.forEach((persona) => {
+  console.log(persona.name);
+})
