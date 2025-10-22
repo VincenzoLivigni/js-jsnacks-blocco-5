ccb5586389dd0990f3ff7ee2f037bbb39d3a2f48
VincenzoLivigni <enzolivigni13@gmail.com>
2025-10-22 14:55:59 +0200

    snack 4 done

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,9 +1,34 @@
+// Crea un nuovo array coi nomi di tutti gli autori
+// Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
 const posts = [
   { author: 'Marco', date: '12/3/2021', content: 'lorem ipsum...' },
   { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
   { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
-]
+];
+console.log(posts);
 
-// Crea un nuovo array coi nomi di tutti gli autori
-// Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+// for
+const result = [];
+for (let i = 0; i < posts.length; i++) {
+     const post = posts[i];
+     // console.log(post);
+     result.push(post.author)
+}
+console.log(result);
+
+
+// forEach
+const empty = [];
+posts.forEach((post) => {
+empty.push(post.author)
+});
+console.log(empty);
+
+// map
+const autore = posts.map((post) => {
+return post.author;
+})
+console.log(autore);
+
+
