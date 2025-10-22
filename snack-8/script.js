5dae474e0ab9ecad54a2d900c0a0b6892523a725
VincenzoLivigni <enzolivigni13@gmail.com>
2025-10-22 16:08:04 +0200

    snack 8 done

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,3 +1,5 @@
+// Recupera la classe dello studente 'Marco Lanci'
+// Risultato: '3C'
 const students = [
   {
     id: 1,
@@ -19,5 +21,28 @@ const students = [
   },
 ];
 
-// Recupera la classe dello studente 'Marco Lanci'
-// Risultato: '3C'
+console.log(students);
+
+
+// for 
+for (let i = 0; i < students.length; i++) {
+     const studente = students[i];
+
+     if (studente.class === "3C") {
+      console.log(studente.class);
+     }
+}
+
+// forEach
+students.forEach((alunno) => {
+       if (alunno.class === "3C") {
+           console.log(alunno.class);
+     }
+})
+
+
+// Find 
+const trovaClasse = students.find((scolaro) => {
+      return scolaro.class === "3C"
+})
+console.log(trovaClasse.class);
