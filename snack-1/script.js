120389e727aa12a5db6ac20ee7550493220f0631
VincenzoLivigni <enzolivigni13@gmail.com>
2025-10-22 14:07:55 +0200

    snack 1 done

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,6 +1,16 @@
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
 const names = ['Edoardo', 'Simone', 'Francesco'];
 console.log(names);
 
+// for
+for (let i = 0; i < names.length; i++) {
+     console.log(names[i]);
+}
 
-// Stampa in console tutti i nomi
-// Risultato: 'Edoardo', 'Simone', 'Francesco'
+// con forEach
+names.forEach((nomi) => {
+    console.log(nomi);
+})
+
